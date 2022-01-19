5ba7260da415d72fd79df1c93f343b89fc98e782
dy27777 <yipdeyao@gmail.com>
2022-01-19 18:58:56 +0800

    initial

---
+++ src/routes/about.svelte
@@ -0,0 +1,23 @@
+<script>
+</script>
+
+<div class="about">
+	<h2>About Us</h2>
+	<p>
+		Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima, deleniti. Dolores eveniet,
+		reiciendis ipsam minima, culpa laborum eum odio perspiciatis sed, unde voluptates amet. Non,
+		ipsum, veritatis vel sed magnam dolore asperiores quod, nam repellat molestiae minima! Magni
+		quisquam, blanditiis quia reprehenderit itaque obcaecati excepturi veniam molestiae illo
+		inventore neque sunt animi dolores et porro nam minus tempora cum! Tenetur.
+	</p>
+	<a href="/guides">View Guides</a>
+	<a href="/">Home</a>
+</div>
+
+<style>
+	.about {
+		text-align: center;
+		display: block;
+		margin: 20px auto;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script>
+</script>
+
+<div class="index">
+	<h2>Welcome</h2>
+	<p>
+		Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae repellat dolores illum tempore
+		voluptatibus ipsam magnam, veritatis exercitationem corrupti ut laudantium blanditiis, mollitia
+		dolore omnis iure corporis reprehenderit possimus nobis est recusandae, reiciendis consequatur
+		autem doloribus explicabo! Animi, inventore. Tempora molestias deserunt excepturi tempore
+		dolorum beatae ipsum repellat unde harum!
+	</p>
+	<a href="/guides">View Guides</a>
+	<a href="/about">About</a>
+</div>
+
+<style>
+	.index {
+		text-align: center;
+		display: block;
+		margin: 20px auto;
+	}
+</style>
